9e341d65d7cbee83f08113df82a365e343359bc3
juandtg21 <juandtg21@gmail.com>
2020-07-05 21:33:43 -0500

    change endpoints

--- src/app/students/student.service.ts
+++ src/app/students/student.service.ts
@@ -13,23 +13,23 @@ export class StudentService {
   constructor(private http:HttpClient) { }
 
   public getStudents():Observable<StudentModel[]>{
-    return this.http.get<StudentModel[]>("http://localhost:8080/GetStudents")
+    return this.http.get<StudentModel[]>("http://localhost:8080/api/GetStudents")
     };
     public deleteStudent(id):Observable<RestResponse>{
       console.log(id);
-      return this.http.delete<RestResponse>(`http://localhost:8080/deleteStudent/${id}`);
+      return this.http.delete<RestResponse>(`http://localhost:8080/api/deleteStudent/${id}`);
       
      }
      
      public getStudent(id){
-       return this.http.get<StudentModel>(`http://localhost:8080/getStudentById/${id}`);
+       return this.http.get<StudentModel>(`http://localhost:8080/api/getStudentById/${id}`);
      
      }
      public updateStudent(student:StudentModel):Observable<RestResponse>{
-      return this.http.post<RestResponse>("http://localhost:8080/updateStudent",JSON.stringify(student));
+      return this.http.post<RestResponse>("http://localhost:8080/api/updateStudent",JSON.stringify(student));
      }
      public createStudent(student:StudentModel):Observable<RestResponse>{
-      return this.http.post<RestResponse>("http://localhost:8080/createStudent",JSON.stringify(student));
+      return this.http.post<RestResponse>("http://localhost:8080/api/createStudent",JSON.stringify(student));
      }
      
      
